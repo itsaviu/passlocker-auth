5906ac05bce51692e08af2cc55c10a0a9315746c
Avinash <avinash.umasankaramoorthy@coda.global>
2020-01-29 01:06:26 +0530

    Validating the user existence in register

--- src/main/java/com/ua/passlocker/auth/service/UserService.java
+++ src/main/java/com/ua/passlocker/auth/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ua.passlocker.auth.service;
 
+import com.ua.passlocker.auth.exceptions.UserAlreadyExistException;
 import com.ua.passlocker.auth.models.dto.RegisterUserReq;
 import com.ua.passlocker.auth.models.dto.UserDetailReq;
 import com.ua.passlocker.auth.models.entity.Users;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,6 +18,9 @@ public class UserService {
     private UsersRepository usersRepository;
 
     public void register(RegisterUserReq registerUserReq) {
+        Optional<Users> user = usersRepository.findByEmailId(registerUserReq.getEmailId());
+        if(user.isPresent())
+            throw new UserAlreadyExistException("User already register for the given email");
         usersRepository.save(new Users(registerUserReq.getUsername(), registerUserReq.getEmailId(), registerUserReq.getPassword(), false, true));
     }
 
